2f768f2a54d488b2cd3514c574f719bc805fc523
Mohammad Mahdi <111296448+GodEye2004@users.noreply.github.com>
2025-10-31 11:45:56 -0700

    ...

--- models.py
+++ models.py
@@ -1,7 +1,21 @@
-from rdflib.tools.csv2rdf import column
-from sqlalchemy import Column, Integer, String, JSON, ForeignKey
+# from rdflib.tools.csv2rdf import column
+# from sqlalchemy import Column, Integer, String, JSON, ForeignKey
+# from sqlalchemy.ext.declarative import declarative_base
+# from traits.trait_types import false
+#
+# Base = declarative_base()
+#
+# class TenatData(Base):
+#     __tablename__ = "ai_assist"
+#
+#     id = Column(Integer, primary_key=True, index=True)
+#     user_id = Column(String, index=True, nullable=false)
+#     category = Column(String, nullable=false)
+#     data = Column(JSON, nullable=False)
+
+
+from sqlalchemy import Column, Integer, String, JSON
 from sqlalchemy.ext.declarative import declarative_base
-from traits.trait_types import false
 
 Base = declarative_base()
 
@@ -9,8 +23,6 @@ class TenatData(Base):
     __tablename__ = "ai_assist"
 
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(String, index=True, nullable=false)
-    category = Column(String, nullable=false)
+    user_id = Column(String, index=True, nullable=False)
+    category = Column(String, nullable=False)
     data = Column(JSON, nullable=False)
-
-

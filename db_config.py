7536e3bc32e06f35b9ea534a6c3ac27987d5c22b
Mohammad Mahdi <111296448+GodEye2004@users.noreply.github.com>
2025-10-31 12:05:40 -0700

    ......

--- db_config.py
+++ db_config.py
@@ -2,7 +2,9 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:mo90mo80@localhost:5432/ai_assist")
+# DATABASE_URL = os.getenv("DATABASE_URL", "postgresql+asyncpg://postgres:mo90mo80@localhost:5432/ai_assist")
+DATABASE_URL = os.getenv("DATABASE_URL", "postgresql://ai_assist_db_user:bRVilvyHJNV83skNohDDag9VcGQ5kshZ@dpg-d40u25f5r7bs73881ur0-a/ai_assist_db")
+
 
 engine = create_async_engine(DATABASE_URL)
 AsyncSessionLocal = sessionmaker(engine, class_=AsyncSession, expire_on_commit=False)
